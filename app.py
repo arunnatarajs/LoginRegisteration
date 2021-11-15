85b02e2e91c02a426e88a59f9f669bedf1224e6b
arun <asaravanan@fleetstudio.com>
2021-11-15 16:47:58 +0530

    final commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect,flash
+
+user=[]
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>HELLO Arun !<p>"
+    return redirect("/home")
 
 @app.route("/home")
 def home():
@@ -18,10 +20,23 @@ def login():
 def register():
     return render_template("register.html")
 
+@app.route("/register",methods=["POST"])
+def register_user():
+    email = request.form.get("mail")
+    password = request.form.get("firstpassword")
+    # user.append([email,password])
+    # print(user)
+    return render_template("loginSuccess.html",value="registered",mail=email)
+
 @app.route("/loginSuccess", methods=["POST"])
 def loginSuccess():
     email = request.form.get('mail')
-    return render_template("loginSuccess.html",mail=email) 
+    password = request.form.get('password')
+    return render_template("loginSuccess.html",value="logged",mail=email)
+    # for i in user:
+    #     if i[0]==email and i[1]==password:
+    #         return render_template("loginSuccess.html",mail=email,password=password)
+    # return "invalid credientials" 
     
 
 if __name__ == "__main__":
